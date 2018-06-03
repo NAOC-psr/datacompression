c8ffd039357746ee1b49319884cc859fc6504d6e
Weiwei Zhu <zhuwwpku@gmail.com>
2018-06-03 22:39:15 +0800

    first working double compression code.

--- runningmean/__init__.py
+++ runningmean/__init__.py
@@ -1,11 +1,14 @@
 from ctypes import *
 import os
+import numpy as np
 PATH, this_file = os.path.split(os.path.realpath(__file__))
 lib = CDLL(PATH + "/runningmean.so")
 
 
-def runningmean(data, outdata, windowsize):
+def runningmean(data, windowsize):
+    outdata = np.zeros(data.shape, dtype='u1')
     nsamp, nchan = data.shape
     #lib.run(data, nsamp, nchan, outdata, windowsize)
     lib.run(c_void_p(data.ctypes.data), c_int(nsamp), c_int(nchan), c_void_p(outdata.ctypes.data), c_int(windowsize))
+    return outdata
 
